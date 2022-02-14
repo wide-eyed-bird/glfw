42ba16607ff32f83aa4268608316cc4db390fb5d
wide-eyed-bird <83553516+wide-eyed-bird@users.noreply.github.com>
2022-02-14 13:25:45 +0100

    Moved staticruntime option to top

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -19,7 +20,6 @@ project "GLFW"
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "Off"
   
         files {
             "src/win32_init.c",
