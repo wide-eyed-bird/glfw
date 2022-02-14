4123c5040745de4df6f395fa9144d861e9f3b884
BuildTools <unconfigured@null.spigotmc.org>
2022-02-14 13:08:11 +0100

    Changed runtime selection

--- premake5.lua
+++ premake5.lua
@@ -38,8 +38,10 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-    filter { "system:windows", "configurations:Debug" }
-        buildoptions "/MTd"
-  
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
