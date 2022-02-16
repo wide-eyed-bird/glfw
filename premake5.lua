b7ebd0af7ed39cc6cee113e35045b26316f0f210
wide-eyed-bird <83553516+wide-eyed-bird@users.noreply.github.com>
2022-02-16 15:49:46 +0100

    Rearranged files

--- premake5.lua
+++ premake5.lua
@@ -9,29 +9,25 @@ project "GLFW"
     files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
         "src/internal.h",
         "src/platform.h",
         "src/mappings.h",
         "src/context.c",
-        "src/platform.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+        "src/platform.c",
         "src/vulkan.c",
         "src/window.c",
-    
-        "src/null_init.c",
-        "src/null_monitor.c",
-        "src/null_window.c",
-        "src/null_joystick.c",
-    
+        "src/egl_context.c",
+        "src/osmesa_context.c",
         "src/null_platform.h",
         "src/null_joystick.h",
-        "src/null_platform.c",
-    
-        "src/egl_context.c",
-        "src/osmesa_context.c"
+        "src/null_init.c",
+
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
 
     filter "system:windows"
@@ -39,20 +35,18 @@ project "GLFW"
         staticruntime "on"
   
         files {
-            "src/win32_time.h",
-            "src/win32_thread.h",
-            "src/win32_platform.h",
-            "src/win32_joystick.h",
-    
             "src/win32_init.c",
+            "src/win32_module.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
+            "src/win32_time.h",
             "src/win32_time.c",
+            "src/win32_thread.h",
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
-    
-            "src/win32_module.c"
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         }
   
         defines {
