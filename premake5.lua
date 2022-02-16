8c14de6701f8462545c4ff06fc57ab4665f9e18b
wide-eyed-bird <83553516+wide-eyed-bird@users.noreply.github.com>
2022-02-16 13:16:48 +0100

    Fixed typo context.h -> context.c

--- premake5.lua
+++ premake5.lua
@@ -10,7 +10,7 @@ project "GLFW"
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
-        "src/context.h",
+        "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
