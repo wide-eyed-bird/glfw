b188c0840a64f58b5857a712fca92b03bc81ce49
wide-eyed-bird <83553516+wide-eyed-bird@users.noreply.github.com>
2022-02-16 15:44:43 +0100

    Added more files

--- premake5.lua
+++ premake5.lua
@@ -20,9 +20,18 @@ project "GLFW"
         "src/monitor.c",
         "src/vulkan.c",
         "src/window.c",
+    
         "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
         "src/null_joystick.c",
+    
+        "src/null_platform.h",
+        "src/null_joystick.h",
         "src/null_platform.c"
+    
+        "src/egl_context.c",
+        "src/osmesa_context.c",
     }
 
     filter "system:windows"
@@ -30,6 +39,11 @@ project "GLFW"
         staticruntime "on"
   
         files {
+            "src/win32_time.h",
+            "src/win32_thread.h",
+            "src/win32_platform.h",
+            "src/win32_joystick.h",
+    
             "src/win32_init.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
@@ -37,8 +51,7 @@ project "GLFW"
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c",
+    
             "src/win32_module.c"
         }
   
